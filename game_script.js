1b6abcc9eafba5b4bbb43fe702087e50069d2331
Eduardo Rosario <eduardorosario25@gmail.com>
2016-05-13 13:47:05 -0700

    changed rob's meow
    meow

--- game_script.js
+++ game_script.js
@@ -186,7 +186,7 @@ var shapeCorrect = new Audio("sounds/Correct.mp3");
 var shapeWrong = new Audio("sounds/Wrong.mp3");
 var pressButton = new Audio("sounds/Button.mp3");
 var meowKitty = new Audio("sounds/Meow.mp3");
-var meowKitty2 = new Audio("sounds/Meow2.mp3");
+var meowKitty2 = new Audio("sounds/Meow3.mp3");
 
 /*****************************/
 /*** HIT REGISTRATION HERE ***/
