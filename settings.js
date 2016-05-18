070a5a4289586e2437131abef91712892c928b80
Eduardo Rosario <eduardorosario25@gmail.com>
2016-05-17 19:46:21 -0700

    aligned settings

--- settings.js
+++ settings.js
@@ -1,13 +1,9 @@
 function popup() {
     w2popup.open({
         title: 'Settings',
-        body: 'Background Music' +
-              '<p></p>' +
-              'Off <input type="range" id="bgmOnOff" max="1" min="0" step="0.01" onchange="changevolume(this.value)"/> On' +
-              '<p></p>' +
-              'Sound Effects' +
-              '<p></p>' +
-              'Off <input type="range" id="soundFXOnOff" max="1" min="0" step="0.01" onchange="changevolume(this.value)"/> On'       
+        body: '<p align="center">Background Music</p>' +
+              '<div align="center"> Off <input type="range" id="bgmOnOff" max="1" min="0" step="0.01" onchange="changevolume(this.value)"/> On</div>'       
+    
     });
 }
 
