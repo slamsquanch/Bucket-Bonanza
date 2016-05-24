9c8f185c08326a9c9278a95ed9c9b225fd5a3e80
Eduardo Rosario <eduardorosario25@gmail.com>
2016-05-23 17:59:26 -0700

    working mute/unmute buttons

--- settings.js
+++ settings.js
@@ -9,5 +9,17 @@ function popup() {
     });
 }
 
-var titleBGM = new Audio('sounds/titleScreenBGM.mp3');
-
+function popupGame() {
+    w2popup.open({
+        title: 'Settings',
+        body: '<p align="center">Background Music</p>' +
+              '<div align="center"> <input type="button" onclick="gameBGM.muted = true; bonanzaMusic.muted = true" value="Mute" /></div>' +
+              '<p></p>' +
+              '<div align="center"> <input type="button" onclick="gameBGM.muted = false; bonanzaMusic.muted = false" value="Unmute" /></div>' +   
+              '<p align="center">Sound Effects</p>' +
+              '<div align="center"> <input type="button" onclick="shapeCorrect.muted = true; shapeWrong.muted = true; meowKitty.muted = true; bonanzaSound.muted = true" value="Mute" /></div>' +
+              '<p></p>' +
+              '<div align="center"> <input type="button" onclick="shapeCorrect.muted = false; shapeWrong.muted = false; meowKitty.muted = false; bonanzaSound.muted = false" value="Unmute" /></div>' 
+    
+    });
+}
