42fffaf805ce7f1888e317de639e9cb076e6cb33
Joanne Hsu <joannehs0@hotmail.com>
2016-05-24 14:00:23 -0700

    Pause menu working minus settings
    The setting icon on the top left corner still needs implementation

--- Scripts/game_script.js
+++ Scripts/game_script.js
@@ -7,6 +7,7 @@ var score;
 var firstCatch;
 
 onload= function() {
+	var elm = document.body; 
 	var windowWidth = window.innerWidth;
 	if(windowWidth > 800){
 		largeBackround();
@@ -50,6 +51,13 @@ onload= function() {
 			objectTimer = null;
 		}
     }
+	var bucketDisable = document.querySelectorAll(".bucket");
+	elm.addEventListener('touchstart', catcher, true);
+	for(i = 0 ; i < 3; i++){
+		if(bucketDisable[i] != null){
+			bucketDisable[i].addEventListener('touchstart', catcher, true);
+		}
+	}
 }
 
 
