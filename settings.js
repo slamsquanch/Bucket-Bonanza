ed65d16c302b1fe2a2b29372f84fd898d3473689
Eduardo Rosario <eduardorosario25@gmail.com>
2016-05-24 10:25:31 -0700

    more changes to settings menu

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 function popup() {
     w2popup.open({
         title: 'Settings',
-        body: '<p align="center">Background Music</p>' +
+        body: '<p align="center" style="font-family:Comic sans MS, cursive, sans-serif;color: orange; font-size:200%">Background Music</p>' +
               '<div align="center"> <input type="button" onclick="titleBGM.muted = true" value="Mute" /></div>' +
               '<p></p>' +
               '<div align="center"> <input type="button" onclick="titleBGM.muted = false" value="Unmute" /></div>'    
@@ -12,11 +12,14 @@ function popup() {
 function popupGame() {
     w2popup.open({
         title: 'Settings',
-        body: '<p align="center">Background Music</p>' +
+        body: '<p></p>' +
+              '<p align="center" style="font-family:Comic sans MS, cursive, sans-serif;color: orange; font-size:200%">Background Music</p>' +
               '<div align="center"> <input type="button" onclick="gameBGM.muted = true; bonanzaMusic.muted = true" value="Mute" /></div>' +
               '<p></p>' +
-              '<div align="center"> <input type="button" onclick="gameBGM.muted = false; bonanzaMusic.muted = false" value="Unmute" /></div>' +   
-              '<p align="center">Sound Effects</p>' +
+              '<div align="center"><input type="button" onclick="gameBGM.muted = false; bonanzaMusic.muted = false" value="Unmute" /></div>' +   
+              '<p></p>' +
+              '<p></p>' +
+              '<p align="center" style="font-family:Comic sans MS, cursive, sans-serif;color: orange; font-size:200%">Sound Effects</p>' +
               '<p></p>' +
               '<div align="center"> <input type="button" onclick="shapeCorrect.muted = true; shapeWrong.muted = true; meowKitty.muted = true; bonanzaSound.muted = true" value="Mute" /></div>' +
               '<p></p>' +
